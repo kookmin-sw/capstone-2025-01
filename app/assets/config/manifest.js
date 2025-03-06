741346bd57eeeaa8be0bb736f693065c6461bd7e
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-03-06 17:44:07 +0900

    Revert "feat: Scss 스타일시트 사용을 위한 설정"

--- app/assets/config/manifest.js
+++
@@ -1,7 +0,0 @@
-// filepath: /Users/geonhye/Desktop/test/my_project/app/assets/config/manifest.js
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link application.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
