1039e1d3c5ad1b95039596e855254cd86cace628
강수민 <sumin6872@naver.com>
2025-04-01 19:18:46 +0900

    fix: 개발 환경에서만 'bills/categories' 경로 활성화

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "bills/categories", to: "bills#categories"
 
     # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
     # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
     # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+    if Rails.env.development?
+      get "bills/categories", to:"bills#categories"
+    end
 
     resources :bills, only: [ :index, :show ] do
       # resources :bill_events, only: [ :index, :show ]
