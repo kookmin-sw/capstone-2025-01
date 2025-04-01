96bf868b3b5242f095b6e5b31260b71a5c077fa1
강수민 <sumin6872@naver.com>
2025-04-01 19:25:35 +0900

    fix: rubocop 오류 해결 - 콜론 뒤 공백 추가

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
     if Rails.env.development?
-      get "bills/categories", to:"bills#categories"
+      get "bills/categories", to: "bills#categories"
     end
 
     resources :bills, only: [ :index, :show ] do
