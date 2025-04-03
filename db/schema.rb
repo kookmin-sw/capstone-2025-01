3b5f893df0dd964914cfd9c48858f2ce5fe30c2d
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-04 03:09:28 +0900

    chore(db): 테이블/컬럼에 주석 추가
    단, sqlite의 경우 주석을 지원하지 않기 때문에 실제 마이그레이션은 하지 않음

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_170950) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_174739) do
   create_table "bills", force: :cascade do |t|
     t.string "title", null: false
     t.string "bill_number"
