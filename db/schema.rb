33884befb02a9aa4e4c50c77a5e820471d50b31d
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-05 19:50:46 +0900

    chore(db): 기록용 코멘트 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_192557) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_05_083200) do
   create_table "bills", force: :cascade do |t|
     t.string "title", null: false
     t.string "bill_number"
