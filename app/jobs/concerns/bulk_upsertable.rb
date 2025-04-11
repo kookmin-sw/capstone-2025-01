09f0be4bddff617e1547924d1c757ead4bbfce9b
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-11 21:21:03 +0900

    feat: Bulk upsert를 위한 ActiveRecord::Import 래핑 BulkUpsertable concern 추가

---
+++ app/jobs/concerns/bulk_upsertable.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+# typed: true
+
+
+module BulkUpsertable
+  extend T::Sig
+  extend ActiveSupport::Concern
+
+  sig {
+    params(
+      records: T::Array[T.untyped], # Array of AR objects, T.untyped for flexibility
+      model_class: T.class_of(ActiveRecord::Base), # Class inheriting from AR::Base
+      conflict_target: T::Array[Symbol],
+      update_columns: T::Array[Symbol],
+      validate: T::Boolean
+    )
+    .void
+  }
+  def bulk_upsert(records, model_class, conflict_target:, update_columns:, validate: false)
+    if records.empty?
+      Rails.logger.info "No records to upsert for #{model_class.name}."
+      return
+    end
+
+    if defined?(ActiveRecord::Import)
+      Rails.logger.info "Upserting #{records.count} #{T.must(model_class.name).pluralize}..."
+      # Ensure updated_at is included for updates
+      columns_to_update = (update_columns.map(&:to_sym) + [ :updated_at ]).uniq
+
+      model_class.import records,
+                          on_duplicate_key_update: {
+                            conflict_target: conflict_target,
+                            columns: columns_to_update
+                          },
+                          validate: validate # Skip validations for performance by default
+    end
+  rescue StandardError => e
+    Rails.logger.error "Bulk upsert failed for #{model_class.name}: #{e.message}\n#{e.backtrace&.first(5)&.join("\n")}"
+    Kernel.raise
+  end
+end
