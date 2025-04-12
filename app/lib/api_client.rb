353b4cb5e9f409e05fbe3894bc24bcad2330a808
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-13 02:49:52 +0900

    fix: User-Agent가 없을 때 공공데이터포털에서 HTTP ROUTING ERROR 응답하는 문제 수정

--- app/lib/api_client.rb
+++ app/lib/api_client.rb
@@ -13,21 +13,31 @@ module ApiClient
 
   DEFAULT_MAX_RETRIES = 3
   DEFAULT_INITIAL_RETRY_DELAY = 5 # seconds
+  DEFAULT_HEADERS = T.let({
+    "Accept": "application/xml",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+  }.freeze, T::Hash[String, String])
 
   sig {
     params(
-      uri: URI::Generic,
+      uri: URI::HTTP,
+      headers: T::Hash[String, String],
       max_retries: Integer,
       initial_delay: Integer
     )
     .returns(String)
   }
-  def self.fetch_with_retry(uri, max_retries: DEFAULT_MAX_RETRIES, initial_delay: DEFAULT_INITIAL_RETRY_DELAY)
+  def self.fetch_with_retry(uri, headers: DEFAULT_HEADERS, max_retries: DEFAULT_MAX_RETRIES, initial_delay: DEFAULT_INITIAL_RETRY_DELAY)
     retries = 0
     delay = initial_delay
     begin
-      Rails.logger.debug { "Fetching URI: #{uri}" } # Use block for lazy evaluation
-      response = T.let(Net::HTTP.get_response(uri), Net::HTTPResponse)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == "https"
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+      headers.each { |key, value| request[key] = value }
+
+      response = T.let(http.request(request), Net::HTTPResponse)
 
       case response
       when Net::HTTPSuccess
