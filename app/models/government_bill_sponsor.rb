8de11c088eb70b0b60527c6a291ea7f15abd5c1c
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-13 20:29:35 +0900

    refactor: `GovernmentBillSponsor` / `NationalAssemblyPerson`에 display_name 메서드 추가

--- app/models/government_bill_sponsor.rb
+++ app/models/government_bill_sponsor.rb
@@ -1,3 +1,12 @@
+# typed: true
+
 class GovernmentBillSponsor < ApplicationRecord
+  extend T::Sig
+
   has_many :proposals, as: :specific_proposer
+
+  sig { returns(String) }
+  def display_name
+    "#{ministry_name} #{department_name} (#{manager_name})"
+  end
 end
