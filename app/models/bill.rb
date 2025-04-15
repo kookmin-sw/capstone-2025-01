767d84489ac9e80fc40c9c8edcd1d54075c4c08b
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-15 22:39:44 +0900

    feat: `BillSummary` 모델 추가 및 `Bill`에 `current_bill_summary` 캐싱컬럼 추가(역정규화)

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,6 +3,9 @@ class Bill < ApplicationRecord
   has_many :proposers, through: :proposals
   has_one :bill_detail
   has_one :government_legislation_notice
+  has_many :bill_summaries, dependent: :destroy
+  # 각 Bill이 여러 bill_summaries 중 유저에게 노출할 것을 빠르게 참조할 수 있도록 역정규화 컬럼(current_bill_summary_id)을 사용
+  belongs_to :current_bill_summary, class_name: "BillSummary", optional: true
 
   validates :bill_type, allow_nil: true, inclusion: { in: %w[헌법개정 예산안 결산 법률안 동의안 승인안 결의안 건의안 규칙안 선출안 중요동의 의원징계 윤리심사 의원자격심사 기타안 기타] }
   validates :assembly_bill_id, uniqueness: true, allow_nil: true
@@ -60,6 +63,14 @@ class Bill < ApplicationRecord
     when "공포", "통과" then "status-passed"
     when "폐기" then "status-discarded"
     else "status-received"
+    end
+  end
+
+  # bill_summaries 추가/삭제 시 current_bill_summary 자동 관리
+  def update_current_bill_summary!(new_summary = nil)
+    if auto_update_current_summary?
+      summary = new_summary || bill_summaries.order(created_at: :desc).first
+      update_column(:current_bill_summary_id, summary&.id)
     end
   end
 end
