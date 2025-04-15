27a46358135439b90828f7d1a5ab769a9a50a28e
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-15 23:01:01 +0900

    chore(db): `bill_summaries.edited_at` 컬럼 제거

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_15_131132) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_15_140018) do
   create_table "bill_details", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -41,7 +41,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_15_131132) do
     t.string "summary_type", default: "llm", null: false
     t.string "llm_model"
     t.integer "editor_id"
-    t.datetime "edited_at"
     t.text "edit_reason"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
