19f3afa152d2554485c89cfb75defccd587ec284
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-04-29 00:15:15 +0900

    refactor: 메인페이지 N+1 쿼리 방지

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     allow_unauthenticated_access  # 👈 인증 없이 접근 허용!
 
     def index
-      @hot_bills = Bill.order(proposed_at: :desc).limit(6)
+      @hot_bills = Bill.includes(proposals: :specific_proposer).order(proposed_at: :desc).limit(6)
     end
 end
