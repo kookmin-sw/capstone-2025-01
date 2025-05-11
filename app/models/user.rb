fbaa7e3ec5a6fffe713d845ea8cf600109784e13
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-05-11 21:58:03 +0900

    chore: User에 email / password validation 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+
+  validates :email_address, presence: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
+  validates :password, on: [ :registration, :password_change ],
+            presence: true,
+            length: { minimum: 8, maximum: 72 }
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
