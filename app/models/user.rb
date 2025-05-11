c5dc1dc1f08e5146216dc15d4e11443c7869758c
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-05-11 22:52:38 +0900

    feat: OAuth 로그인 처리

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,22 @@ class User < ApplicationRecord
             length: { minimum: 8, maximum: 72 }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+
+  def self.create_from_oauth(auth)
+    email = auth.info.email
+    user = self.new email_address: email, password: SecureRandom.base64(64).truncate_bytes(64)
+    user.assign_name_from_auth(auth)
+    user.save
+    user
+  end
+
+  def signed_in_with_oauth(auth)
+    assign_name_from_auth(auth)
+    save if name_changed?
+  end
+
+  def assign_name_from_auth(auth)
+    self.name = auth.info.name if auth.info.name.present?
+  end
 end
