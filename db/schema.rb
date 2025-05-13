9621fc7aed46cf3f2405e7c064d29972436605b5
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2025-05-14 00:20:55 +0900

    feat: User에 어드민 구분을 위한 컬럼 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_13_142221) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_13_151706) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -192,6 +192,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_13_142221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
